062f7fc5220828d90327766ad5eedca92ab5ed30
Richard <richard@smith-net.org.uk>
2021-01-01 21:32:00 +0000

    Tidy up some warnings around use of == rather than ===

--- src/components/Selectable.js
+++ src/components/Selectable.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export default (props) => {
 
     //store the index of the item that was select last - used as the origin point for a shift-select
-    const [lastSelectedItem, setLastSelectedItem] = useState();
+    const [lastSelectedItem, setLastSelectedItem] = React.useState();
 
     //get the key prop for each item in the selectable zone
     const selectableItemKeys = props.children.map(child => child.key);
@@ -18,19 +18,19 @@ export default (props) => {
         let newSelection = [];
 
         //if it's a standard unmodified click - make the selection just this item
-        if (event.shiftKey == false) {
+        if (event.shiftKey === false) {
             newSelection.push(this.key);
         }
 
         //if we are using a shift modifier then get all the items between the last selected item and the item that was just clicked
-        if (event.shiftKey == true && lastSelectedItem !== undefined) {
+        if (event.shiftKey === true && lastSelectedItem !== undefined) {
             for (var i = Math.min(lastSelectedItem, thisSelectedItem); i <= Math.max(lastSelectedItem, thisSelectedItem); i++) {
                 newSelection.push(selectableItemKeys[i])
             }
         }
 
         //add or remove the clicked child into our list
-        if (event.ctrlKey == true) {
+        if (event.ctrlKey === true) {
 
             //if the item has already been selected, then remove it from the selection
             if (props.selectedItems.includes(this.key)) {
@@ -49,14 +49,14 @@ export default (props) => {
         }
 
         //ctrl adds the new items to the selection - no ctrl replaces the entire selection
-        if (event.ctrlKey == true) {
+        if (event.ctrlKey === true) {
             props.onSelectionChange([...props.selectedItems, ...newSelection]);
         } else {
             props.onSelectionChange(newSelection);
         }
 
         //...and then update it for next time?
-        if (event.shiftKey == false) {
+        if (event.shiftKey === false) {
             setLastSelectedItem(thisSelectedItem);
         }
 
