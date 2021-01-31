77b560aebb2f2c3c5884ec78cebf4a8e02ef2821
Richard <richard@smith-net.org.uk>
2021-01-31 21:42:38 +0000

    Tweak the greebles on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ export default (props) => {
     return (
         <header>
             <svg>
-                <circle style={{ "fill" : "#6894cc", "stroke" : "#fff", "strokeWidth" : "8px" }} cx="41" cy="41" r="36" />
-                <circle style={{ "fill" : "#f94848", "stroke" : "#333", "strokeWidth" : "3px" }} cx="127.5" cy="17.5" r="13" />
-                <circle style={{ "fill" : "#fcab1d", "stroke" : "#333", "strokeWidth" : "3px" }} cx="173.5" cy="17.5" r="13" />
-                <circle style={{ "fill" : "#87c13a", "stroke" : "#333", "strokeWidth" : "3px" }} cx="219.5" cy="17.5" r="13" />
+                <circle style={{ fill:"#6894cc", stroke:"#ffffff", strokeWidth:"8px" }} cx="40"  cy="40" r="32" />
+                <circle style={{ fill:"#f94848", stroke:"#333333", strokeWidth:"4px" }} cx="115" cy="21" r="15" />
+                <circle style={{ fill:"#fcab1d", stroke:"#333333", strokeWidth:"4px" }} cx="160" cy="21" r="15" />
+                <circle style={{ fill:"#87c13a", stroke:"#333333", strokeWidth:"4px" }} cx="205" cy="21" r="15" />
             </svg>
         </header>
     );
