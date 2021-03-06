ea15be4969d3cc293334205d60f69f8f9d7e5b17
Richard <richard@smith-net.org.uk>
2021-03-06 19:37:28 +0000

    Basic from utility system - provider boilerplate state management on forms

---
+++ src/components/form/Button.js
@@ -0,0 +1,14 @@
+
+//
+import React from 'react';
+
+//
+export default function (props) {
+    return (
+        <React.Fragment>
+            <label>
+                <button type="button" name="">Button</button>
+            </label>
+        </React.Fragment>
+    );
+};
---
+++ src/components/form/Submit.js
@@ -0,0 +1,15 @@
+
+//
+import React from 'react';
+
+//
+export default function (props) {
+    return (
+        <React.Fragment>
+            <label>
+                
+                <button type="submit" name="">Submit</button>
+            </label>
+        </React.Fragment>
+    );
+};
