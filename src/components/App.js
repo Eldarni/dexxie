2fdf0b5191c8b5f38c5f73b583eafabb347954bd
Richard <richard@smith-net.org.uk>
2021-03-07 10:19:18 +0000

    store the current profile selection and refactor the profile data logic

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ import React, { useState, useEffect } from 'react';
 import pokemonList from '../static/pokemon.json';
 
 //
+import usePersistedState from '../hooks/usePersistedState';
+
+//
 import Header  from './Header';
 import Content from './Content';
 import Footer  from './Footer';
@@ -25,15 +28,11 @@ import { ProfileProvider }  from '../context/ProfileContext';
 //
 export default () => {
 
-    //store the user's data in state
-    const [userData, setUserData] = useState(() => {
-        return JSON.parse(window.localStorage.getItem('user-data')) || {};
-    });
+    //what profile are we using
+    const [currentProfile, setCurrentProfile] = usePersistedState('current-profile', 'ad75aad9-5373-4581-b8e1-5819aaef283a');
 
-    //save the user's data to local storage
-    useEffect(() => {
-        window.localStorage.setItem('user-data', JSON.stringify(userData));
-    });
+    //load the user data that's been save for the current profile
+    const [userData, setUserData] = usePersistedState('profile-' + currentProfile, {});
 
     //merge the contents of the user's personal tags into the main tag list
     Object.keys(userData).forEach(function(pokemon) {
