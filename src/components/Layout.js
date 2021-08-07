b0d6cd2ca72b7748bfec1ca7858cfbdfb66d7cf8
RickBaby <richard@smith-net.org.uk>
2021-08-07 22:23:02 +0100

    Use a single layout file rather than separate header/content/footer components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,25 @@
-import React from "react"
-import PropTypes from "prop-types"
 
-const Layout = (props) => (
+//
+import React from "react"
+
+//
+export default (props) => (
     <React.Fragment>
-        {props.children}
+
+        <header>
+            <svg>
+                <circle style={{ fill:"#6894cc", stroke:"#ffffff", strokeWidth:"8px" }} cx="40"  cy="40" r="32" />
+                <circle style={{ fill:"#f94848", stroke:"#333333", strokeWidth:"4px" }} cx="115" cy="21" r="15" />
+                <circle style={{ fill:"#fcab1d", stroke:"#333333", strokeWidth:"4px" }} cx="160" cy="21" r="15" />
+                <circle style={{ fill:"#87c13a", stroke:"#333333", strokeWidth:"4px" }} cx="205" cy="21" r="15" />
+            </svg>
+        </header>
+
+        <main className={props.className}>{props.children}</main>
+
+        <footer>
+            <h1>Dexxie</h1>
+        </footer>
+
     </React.Fragment>
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default Layout;
