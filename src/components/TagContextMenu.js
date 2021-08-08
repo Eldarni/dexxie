2f3366a1b7db3a0ea5a873a8aaf827a62c36ca59
RickBaby <richard@smith-net.org.uk>
2021-08-08 14:08:04 +0100

    Add a system to determin if a tag appears on all or some (or none) of the selected pokemon and shows the correct state on the context menu

--- src/components/TagContextMenu.js
+++ src/components/TagContextMenu.js
@@ -5,16 +5,33 @@ import React from 'react';
 //
 import ContextMenu, { ContextMenuItem, ContextMenuDivider } from './ContextMenu';
 
-
 //
 export default (props) => {
+
+    //all i need in this is a array - so lets convert our selectedPokemon into an array
+    const selectedPokemon = Object.values(props.selectedPokemon);
+
+    //for now, hardcode a list of tags
+    const tags = [
+        {'id':'8c8483fc-bf64-48ae-965d-cde56e884b46', 'label': 'Owned'}, 
+        {'id':'83b729b5-14c7-436d-8a0b-d61091b5cd3e', 'label': 'Shiny'}, 
+        {'id':'457e7d10-f51d-48b2-9ede-4074c3b26872', 'label': 'Lucky'}
+    ];
+
+    //
+    tags.forEach((tag) => {
+        const selectedPokemonWithTag = selectedPokemon.filter((pokemon) => {
+            return pokemon.tags.includes(tag.label);
+        });
+        tag.appears = ((selectedPokemonWithTag.length == selectedPokemon.length) ? 'all' : ((selectedPokemonWithTag.length > 0) ? 'some' : 'never'));
+    });
 
     //
     return (
         <ContextMenu selector={props.selector}>
-            <ContextMenuItem selected>Owned</ContextMenuItem>
-            <ContextMenuItem>Shiny</ContextMenuItem>
-            <ContextMenuItem>Lucky</ContextMenuItem>
+            {tags.map((tag) => { 
+                return (<ContextMenuItem key={tag.id} className={((tag.appears == 'some') ? 'indeterminate' : null)} selected={(tag.appears == 'all' || tag.appears == 'some')}>{tag.label}</ContextMenuItem>);
+            })}
             <ContextMenuDivider />
             <ContextMenuItem>Clear All Tags</ContextMenuItem>
         </ContextMenu>
