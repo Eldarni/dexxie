3bc773c69178a3bccb0dff00e71cc5d5a375dfbc
RickBaby <richard@smith-net.org.uk>
2021-08-08 21:23:59 +0100

    I can spell good

--- src/components/WelcomePopup.js
+++ src/components/WelcomePopup.js
@@ -16,13 +16,13 @@ export default (props) => {
             <p>This means that you may experience bugs, half implemented features or just plain missing functionality. It also means that any Pokémon that you tag, profiles you set up may be lost as the app is updated.</p>
 
             <h3>So what is this anyway?</h3>
-            <p>It's a Pokédex - a place for you to record which Pokémon you have, what shinies you own etc. This idea was origionally born from my frustrations with the Pokédex feature in Pokémon GO - with it's lack of search options and categorisation, I find it hard to determine what shinies or lucky pokemon I am missing.</p>
+            <p>It's a Pokédex - a place for you to record which Pokémon you have, what shinies you own etc. This idea was originally born from my frustrations with the Pokédex feature in Pokémon GO - with it's lack of search options and categorisation, I find it hard to determine what shinies or lucky pokemon I am missing.</p>
 
             <h3>Does this not already exist?</h3>
             <p>I have found a few apps that attempt to solve my issues, but most - imo - did not have ability for me to customise the tags or what Pokémon were listed, I don't really care about tracking every Pikachu in a costume that Niantic releases, but some do, so why should I (or you) be restricted in what you want to record. To that end I want to be able to let you define your own tags, and your own profiles - these will control which Pokémon are listed - only play Pokémon Red - great - you will be able to filter the list to just the Pokémon found in that version.</p>
 
             <h3>How do I report an issue?</h3>
-            <p>Sorry about that, but thanks for wanting to let me know, the best way to report it is on my <a href="https://github.com/eldarni/dexxie">GitHub project page</a>, there is an issue tracker that you can post a report, please try to provide as much infomration as you can, what you were trying to do - the more information you can provide, the more likely it is that I or someone else can replicate it, and a but that that is replicatable is a dead bug.</p>
+            <p>Sorry about that, but thanks for wanting to let me know, the best way to report it is on my <a href="https://github.com/eldarni/dexxie">GitHub project page</a>, there is an issue tracker that you can post a report, please try to provide as much information as you can, what you were trying to do - the more information you can provide, the more likely it is that I or someone else can replicate it, and a bug that that is replicable is a dead bug.</p>
 
             <p><i>Eldarni</i></p>
 
