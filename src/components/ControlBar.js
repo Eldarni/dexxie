b1d0f99ff621053b1acab5d8f4572114f9334f64
RickBaby <richard@smith-net.org.uk>
2021-08-22 17:50:40 +0100

    Update the "SearchBar" element to include a future method for switching between and managing profiles

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -4,19 +4,14 @@ import React from 'react';
 
 //
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleUp, faCheckDouble, faTimes, faTh, faThLarge, faSquare } from '@fortawesome/free-solid-svg-icons'
-
-//
-import ProfileMenuWidget from './ProfileMenuWidget';
+import { faAngleUp, faCheckDouble, faTimes, faTh, faThLarge, faSquare, faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 //
 export default (props) => {
-    
+
     //
     return (
         <div className="control-bar">
-
-            <ProfileMenuWidget currentProfile={props.currentProfile} setShowProfilesPopop={props.setShowProfilesPopop} />
 
             <div className="control-group">
                 <div className="control-button" onClick={props.handleSelectAll} title="Select all"><FontAwesomeIcon size="lg" icon={faCheckDouble} /></div>
@@ -24,7 +19,10 @@ export default (props) => {
             </div>
 
             <div className="control-group">
-                <input type="search" onChange={props.search} value={props.searchString} placeholder="Search: e.g. kanto grass" />
+                <div class="SearchBar">
+                    <div class="ProfileSwitcher">National Dex <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon></div>
+                    <input type="search" onChange={props.search} value={props.searchString} placeholder="Search: e.g. kanto grass" />
+                </div>
             </div>
 
             <div className="control-group">
