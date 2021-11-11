808e3c125e8238fba325db0c1b60d34c01fe1823
RickBaby <richard@smith-net.org.uk>
2021-11-11 21:55:16 +0000

    update the tag menu to use recoil + flip behaviour when selecting a "indeterminate" tag

--- src/components/TagContextMenu.js
+++ src/components/TagContextMenu.js
@@ -1,13 +1,10 @@
 
 //
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 //
-import { userTagsState } from '../store';
-
-//
-import { useProfileState }  from '../context/ProfileContext';
+import { currentProfileDataState, userTagsState } from '../store';
 
 //
 import ContextMenu, { ContextMenuItem, ContextMenuDivider } from './ContextMenu';
@@ -16,9 +13,7 @@ import ContextMenu, { ContextMenuItem, ContextMenuDivider } from './ContextMenu'
 export default (props) => {
 
     //
-    const profileState = useProfileState();
-
-    //
+    const [currentProfile, setCurrentProfile] = useRecoilState(currentProfileDataState);
     const userTags = useRecoilValue(userTagsState);
 
     //all i need in this is a array - so lets convert our selectedPokemon into an array
@@ -32,29 +27,52 @@ export default (props) => {
             return pokemon.tags.includes(tag.tag);
         });
 
-        //
-        let updateSelectedPokemonTags = null, appears = false;
-
-        //assign an appropiate handler to update the tags on the selected pokemon (add the tag, unless all the pokemon already have the tag)
-        if (selectedPokemonWithTag.length !== selectedPokemon.length) {
-            updateSelectedPokemonTags = () => { profileState.addTagToPokemon(selectedPokemon, tag.tag); };
-        } else {
-            updateSelectedPokemonTags = () => { profileState.removeTagFromPokemon(selectedPokemon, tag.tag); };
-        }
-
-        //
-        appears = ((selectedPokemonWithTag.length === selectedPokemon.length) ? 'all' : ((selectedPokemonWithTag.length > 0) ? 'some' : 'never'));
-
-        //
-        return { ...tag, updateSelectedPokemonTags, appears };
+        //merge in an flag to indicate if this tag appears none of the time, some of the time or all of the time
+        return { ...tag, 'appears': ((selectedPokemonWithTag.length === selectedPokemon.length) ? 'all' : ((selectedPokemonWithTag.length > 0) ? 'some' : 'none')) };
 
     });
+
+    //
+    const addTagToSelection = (tag) => {
+
+        //
+        let newTagData = {};
+
+        //do this with no mutation!
+        Object.keys(props.selectedPokemon).map((pokemon) => {
+            if (!props.selectedPokemon[pokemon].tags.includes(tag)) {
+                newTagData[pokemon] = [ ...props.selectedPokemon[pokemon].tags, tag ];
+            } else {
+                newTagData[pokemon] = props.selectedPokemon[pokemon].tags;
+            }
+        });
+
+        //
+        setCurrentProfile({ ...currentProfile, 'tags': { ...currentProfile.tags, ...newTagData }});
+
+    }
+
+    //
+    const removeTagFromSelection = (tag) => {
+
+        //
+        let newTagData = {};
+
+        //do this with no mutation!
+        Object.keys(props.selectedPokemon).map((pokemon) => {
+            newTagData[pokemon] = props.selectedPokemon[pokemon].tags.filter((v) => v !== tag);
+        });
+
+        //
+        setCurrentProfile({ ...currentProfile, 'tags': { ...currentProfile.tags, ...newTagData }});
+
+    }
 
     //
     return (
         <ContextMenu selector={props.selector}>
             {tags.map((tag) => { 
-                return (<ContextMenuItem key={tag.id} onClick={tag.updateSelectedPokemonTags} className={((tag.appears === 'some') ? 'indeterminate' : null)} selected={(tag.appears === 'all' || tag.appears === 'some')}>{tag.tag}</ContextMenuItem>);
+                return (<ContextMenuItem key={tag.id} onClick={() => (tag.appears === 'none') ? addTagToSelection(tag.tag) : removeTagFromSelection(tag.tag) } className={((tag.appears === 'some') ? 'indeterminate' : null)} selected={(tag.appears === 'all' || tag.appears === 'some')}>{tag.tag}</ContextMenuItem>);
             })}
             <ContextMenuDivider />
             <ContextMenuItem>Clear All Tags</ContextMenuItem>
