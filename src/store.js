1f62b84c32a2c837145f55573be390c33fc8c28f
RickBaby <richard@smith-net.org.uk>
2021-11-11 11:53:30 +0000

    add local storage persistence to stored atoms (pretty much a copy-paste from the recoil docs)

--- src/store.js
+++ src/store.js
@@ -3,7 +3,32 @@
 import { atom } from 'recoil';
 
 //
+const localStorageEffect = (key) => {
+    return ({setSelf, onSet}) => {
+
+        //
+        const savedValue = localStorage.getItem(key)
+        if (savedValue != null) {
+            setSelf(JSON.parse(savedValue));
+        }
+
+        //
+        onSet((newValue, oldValue, isReset) => {
+            if (isReset) {
+                localStorage.removeItem(key);
+            } else {
+                localStorage.setItem(key, JSON.stringify(newValue));
+            }
+        });
+
+    };
+};
+
+//
 export const displayModeState = atom({
     'key'     : 'displayMode',
     'default' : 'standard',
+    effects_UNSTABLE: [
+        localStorageEffect('displayMode'),
+    ]
 });
