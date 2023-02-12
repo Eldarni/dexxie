f6c40cd99a78eba57202c4b2bd35e6b6366e019e
RickBaby <richard@smith-net.org.uk>
2023-02-12 21:45:08 +0000

    resize the sprite to 256x256 before saving

--- dev/download-sprites.mjs
+++ dev/download-sprites.mjs
@@ -20,9 +20,9 @@ zip.getEntries().forEach(function(entry) {
     //see if the sprite path and filename matches our desired icon
     const matches = entry.entryName.match(/sprites-master\/sprites\/pokemon\/other\/home\/(?:(shiny)\/)?([0-9]+).png/);
 
-    //extract the sprite, and convert it to webp and put it into the public/isoncs folder
+    //extract the sprite, and convert it to webp and put it into the public/icons folder
     if (matches !== null) {
-        sharp(entry.getData()).webp().toFile(`../public/icons/${matches[2]}${((matches[1] == 'shiny') ? '-shiny' : '')}.webp`);
+        sharp(entry.getData()).resize(256, 256).webp().toFile(`../public/icons/${matches[2]}${((matches[1] == 'shiny') ? '-shiny' : '')}.webp`);
     }
 
 });
