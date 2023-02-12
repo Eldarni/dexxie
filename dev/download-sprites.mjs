db72582df2caaefa2f257572e49639901f102a8e
RickBaby <richard@smith-net.org.uk>
2023-02-12 21:33:42 +0000

    add module to download the sprites from the PokeAPI/sprites repo on Github

---
+++ dev/download-sprites.mjs
@@ -0,0 +1,31 @@
+
+//
+import fs from 'fs';
+import sharp from 'sharp';
+import AdmZip from 'adm-zip';
+
+//download the sprites from github
+const response = await fetch('https://codeload.github.com/PokeAPI/sprites/zip/master');
+const buffer = Buffer.from(await response.arrayBuffer());
+
+//
+fs.writeFileSync('./sprites.zip', buffer);
+
+//open the zip file
+const zip = new AdmZip('./sprites.zip');
+
+//
+zip.getEntries().forEach(function(entry) {
+
+    //see if the sprite path and filename matches our desired icon
+    const matches = entry.entryName.match(/sprites-master\/sprites\/pokemon\/other\/home\/(?:(shiny)\/)?([0-9]+).png/);
+
+    //extract the sprite, and convert it to webp and put it into the public/isoncs folder
+    if (matches !== null) {
+        sharp(entry.getData()).webp().toFile(`../public/icons/${matches[2]}${((matches[1] == 'shiny') ? '-shiny' : '')}.webp`);
+    }
+
+});
+
+//
+fs.unlink('./sprites.zip');
