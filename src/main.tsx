8ea94be9ae58ea724a22e060183ae5a1dda09c0f
RickBaby <richard@smith-net.org.uk>
2023-02-19 13:49:00 +0000

    Migrate state init out of the store and into main

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 //use recoil
-import { RecoilRoot } from 'recoil'
+import { MutableSnapshot, RecoilRoot } from 'recoil'
 
 //include the main stylesheet
 import './scss/style.scss'
@@ -13,9 +13,38 @@ import './scss/style.scss'
 import App from './components/App'
 
 //
+import { currentProfileState, displayModeState, userProfilesState, userTagsState } from './store'
+
+//
+const initializeRecoilState = ({set}: MutableSnapshot): void => {
+
+    //
+    set(displayModeState, 'standard')
+
+    //
+    set(currentProfileState, '92503e70-c4ca-42d2-9a06-9f26870e66c3')
+
+    //
+    set(userTagsState, [
+        { 'id' : 'e7242c83-34f5-4ff9-84f4-38e1d0169b3b', 'tag' : 'Owned' },
+        { 'id' : '891f5a8f-db93-4f5f-a219-f721ca8db723', 'tag' : 'Shiny' },
+        { 'id' : '279f916c-247c-4f48-ac63-205145629b95', 'tag' : 'Lucky' },
+    ])
+    
+    //
+    set(userProfilesState('92503e70-c4ca-42d2-9a06-9f26870e66c3'), {
+        'icon': '/icons/151-mew.svg',
+        'name': 'National Dex',
+        'filter': null,
+        'tags': {}
+    })
+
+};
+
+//
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <RecoilRoot>
+        <RecoilRoot initializeState={initializeRecoilState}>
             <App />
         </RecoilRoot>
     </StrictMode>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 
 //
-import { atom, atomFamily, selector } from 'recoil';
+import { atom, atomFamily, selector } from 'recoil'
 
 //
 const localStorageEffect = () => {
@@ -9,92 +9,78 @@ const localStorageEffect = () => {
         //
         const savedValue = localStorage.getItem(node.key)
         if (savedValue != null) {
-            setSelf(JSON.parse(savedValue));
+            setSelf(JSON.parse(savedValue))
         }
 
         //
         onSet((newValue, oldValue, isReset) => {
             if (isReset) {
-                localStorage.removeItem(node.key);
+                localStorage.removeItem(node.key)
             } else {
-                localStorage.setItem(node.key, JSON.stringify(newValue));
+                localStorage.setItem(node.key, JSON.stringify(newValue))
             }
-        });
+        })
 
-    };
-};
+    }
+}
 
 //
 export const displayModeState = atom({
-    'key'     : 'displayMode',
-    'default' : 'standard',
+    'key': 'displayMode',
     effects_UNSTABLE: [
         localStorageEffect(),
     ]
-});
+})
 
 //
 export const currentProfileState = atom({
-    'key'     : 'currentProfile',
-    'default' : '92503e70-c4ca-42d2-9a06-9f26870e66c3',
+    'key': 'currentProfile',
     effects_UNSTABLE: [
         localStorageEffect(),
     ]
-});
-
-//
-const userTagsInitialState = [
-    { 'id' : 'e7242c83-34f5-4ff9-84f4-38e1d0169b3b', 'tag' : 'Owned' },
-    { 'id' : '891f5a8f-db93-4f5f-a219-f721ca8db723', 'tag' : 'Shiny' },
-    { 'id' : '279f916c-247c-4f48-ac63-205145629b95', 'tag' : 'Lucky' }
-];
+})
 
 //
 export const userTagsState = atom({
-    'key'     : 'userTags',
-    'default' : userTagsInitialState,
+    'key': 'userTags',
     effects_UNSTABLE: [
         localStorageEffect(),
     ]
-});
-
-//
-const userProfilesInitialState = { 'icon': '/icons/151-mew.svg', 'name': 'National Dex', 'filter': null, 'tags': {} };
+})
 
 //
 export const userProfilesState = atomFamily({
-    'key'     : 'userProfiles',
-    'default' : userProfilesInitialState,
+    'key': 'userProfiles',
     effects_UNSTABLE: [
         localStorageEffect(),
     ]
-});
+})
 
 //
 export const currentProfileDataState = selector({
     'key': 'currentProfileData',
     'get': ({get}) => {
-        return get(userProfilesState(get(currentProfileState)));
+        return get(userProfilesState(get(currentProfileState)))
     },
     'set': ({set, get}, newValue) => {
         return set(userProfilesState(get(currentProfileState)), newValue)
     },
-});
+})
 
 //
 export const selectedPokemonState = atom({
-    'key'     : 'selectedPokemon',
+    'key': 'selectedPokemon',
     'default' : [],
-});
+})
 
 //
 export const selectedPokemonTagsState = selector({
     'key': 'selectedPokemonTags',
     'get': ({get}) => {
-        const currentProfile  = get(currentProfileDataState);
-        const selectedPokemon = get(selectedPokemonState);
+        const currentProfile  = get(currentProfileDataState)
+        const selectedPokemon = get(selectedPokemonState)
         return selectedPokemon.reduce((previous, pokemon) => {
             return { ...previous, [pokemon]: ((currentProfile.tags.hasOwnProperty(pokemon) ) ? currentProfile.tags[pokemon] : [])}
-        }, {});
+        }, {})
     },
-});
+})
