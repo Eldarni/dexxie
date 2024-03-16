f0366284aaabea49c6ede6e6e6ca1433b6adc762
Eldarni <hello@eldarni.me>
2024-03-16 21:14:50 +0000

    basic setup of app + initial layout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+//
+import React from "react"
+
+//
+import Layout from './layout/Layout'
+
+//
+export default function() {
+
+    //
+    return (
+        <Layout>
+            <h1>Dexxie</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius ullam cupiditate sunt modi laboriosam alias! Ipsam suscipit molestiae corporis, beatae asperiores molestias repellat, pariatur voluptas totam aperiam deleniti fugiat.</p>
+        </Layout>
+    )
+
+}
