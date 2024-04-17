751c334d05cfb0ed6b14498be8ee764353e757e9
Eldarni <hello@eldarni.me>
2024-04-17 22:41:04 +0100

    time to start again

--- README.md
+++
@@ -1,3 +0,0 @@
-# PokeDex
-
-Yet Another Pokedex Project
--- src/utils/concatUnique.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * 
- * @param {*} array1 
- * @param {*} array2 
- */
-export default (array1, array2) => {
-    return array1.concat(array2.filter((item) => array1.indexOf(item) < 0))
-};
