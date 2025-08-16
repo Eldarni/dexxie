f27605b486220508d7f97bb793c6a9aa4369e6ee
RickBaby <richard@smith-net.org.uk>
2025-08-16 21:08:32 +0100

    add the core to the popup system

---
+++ src/popup.mjs
@@ -0,0 +1,72 @@
+//
+import { addEventListener } from '../utilities/addEventListener.mjs';
+
+//
+export async function showPopup(title, callback) {
+
+    //
+    const existingPopup = document.querySelector('.popup-overlay');
+    if (existingPopup) {
+        existingPopup.remove();
+    }
+    
+    //
+    const popup = document.createRange().createContextualFragment(`
+        <div class="popup-overlay">
+            <div class="popup-content">
+                <div class="popup-header">
+                    <h2>${title}</h2>
+                    <button class="popup-close">&times;</button>
+                </div>
+                <div class="popup-body"></div>
+            </div>
+        </div>
+    `);
+
+    //
+    if (typeof callback === 'function') {
+        await callback(popup.querySelector('.popup-body'));
+    }
+
+    //
+    popup.querySelector('button.popup-close').addEventListener('click', hidePopup);
+
+    //
+    const mainContainer = document.querySelector('main');
+    mainContainer.appendChild(popup);
+    
+    //dismiss popup when clicking outside of the popup
+    addEventListener({ on: '.popup-overlay', event: 'click', callback: (event) => {
+        if (event.target.closest('.popup-content') === null) {
+            console.log('removing dismiss popup on click listener');
+            hidePopup();
+        }
+    }});     
+
+    //get the drawer to animate in (done on next frame as the animation skips frames for some reason)
+    setTimeout(() => {
+        document.querySelector('.popup-content').setAttribute('data-show', 'true');
+    }, 1);
+    
+}
+
+//
+export function hidePopup() {
+
+    //
+    const popup = document.querySelector('.popup-overlay');
+
+    //
+    if (!popup) {
+        return;
+    }
+
+    //
+    popup.querySelector('.popup-content').setAttribute('data-hide', 'true');
+        
+    //
+    popup.addEventListener('animationend', (event) => { 
+        popup.remove();
+    });
+
+}
